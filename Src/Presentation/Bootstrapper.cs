9110eb7f9c991166f6c6e0bf34780456daf0d69f
Philip Mørch <mrphil2105@gmail.com>
2022-10-31 01:22:23 +0100

    Create module for views and view models

--- Src/Presentation/Bootstrapper.cs
+++ Src/Presentation/Bootstrapper.cs
@@ -1,8 +1,13 @@
 ﻿using MvvmElegance;
+using SourceServerManager.Presentation.Modules;
 using SourceServerManager.ViewModels;
 
 namespace SourceServerManager.Presentation;
 
 public class Bootstrapper : AutofacBootstrapper<MainViewModel>
 {
+    protected override void ConfigureServices(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyModules(typeof(ViewModule).Assembly);
+    }
 }
