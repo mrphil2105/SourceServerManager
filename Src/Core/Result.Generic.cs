345867903c85b80f652a801050a2c85057e4449a
Philip Mørch <mrphil2105@gmail.com>
2022-11-02 21:00:48 +0100

    Create Result base classes

---
+++ Src/Core/Result.Generic.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SourceServerManager.Core;
+
+public abstract class Result<T> : Result
+{
+    private readonly T _data;
+
+    protected Result(T data, string? message) : base(message)
+    {
+        _data = data;
+    }
+
+    public T Data => Success
+        ? _data
+        : throw new InvalidOperationException($"Unable to access {nameof(Data)} when {nameof(Success)} is false.");
+
+    public static Result<T> FromResult(Result result, [DisallowNull] T data)
+    {
+        return result.Success ? new SuccessResult<T>(data) : new ErrorResult<T>(result.Message);
+    }
+}
---
+++ Src/Core/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace SourceServerManager.Core;
+
+public abstract class Result
+{
+    protected Result(string? message)
+    {
+        Message = message;
+    }
+
+    public abstract bool Success { get; }
+
+    public bool Failure => !Success;
+
+    public string? Message { get; }
+}
