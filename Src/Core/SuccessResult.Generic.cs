03677efcfa16c13a0364e99b3044d765ad45c912
Philip Mørch <mrphil2105@gmail.com>
2022-11-02 21:01:07 +0100

    Implement SuccessResult classes

---
+++ Src/Core/SuccessResult.Generic.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SourceServerManager.Core;
+
+public class SuccessResult<T> : Result<T>
+{
+    public SuccessResult([DisallowNull] T data, string? message = null) : base(data, message)
+    {
+        if (data is null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+    }
+
+    public override bool Success => true;
+}
---
+++ Src/Core/SuccessResult.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceServerManager.Core;
+
+public class SuccessResult : Result
+{
+    public SuccessResult(string? message = null) : base(message)
+    {
+    }
+
+    public override bool Success => true;
+}
