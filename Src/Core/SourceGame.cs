3fc8cb4d8e82d87ecb3f54c51e331ab5b9ba0d24
Philip Mørch <mrphil2105@gmail.com>
2022-11-02 01:19:48 +0100

    Create SourceGame enumeration

---
+++ Src/Core/SourceGame.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SourceServerManager.Core;
+
+public enum SourceGame
+{
+    [Description("Team Fortress 2")]
+    Tf2,
+    [Description("Counter-Strike: Global Offensive")]
+    Csgo,
+    [Description("Counter-Strike: Source")]
+    Css,
+    [Description("Left 4 Dead 2")]
+    L4d2
+}
