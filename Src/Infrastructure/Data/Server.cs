067063e4264b5ad93aaa40df2d7c256bea5de3a3
Philip Mørch <mrphil2105@gmail.com>
2022-11-02 01:25:39 +0100

    Create Server entity

---
+++ Src/Infrastructure/Data/Server.cs
@@ -0,0 +1,38 @@
+﻿namespace SourceServerManager.Infrastructure.Data;
+
+public class Server
+{
+    public Server(SourceGame game, string hostname, string directoryName, string startupMap, int maxPlayerCount,
+        string address, int port)
+    {
+        Game = game;
+        Hostname = hostname;
+        DirectoryName = directoryName;
+        StartupMap = startupMap;
+        MaxPlayerCount = maxPlayerCount;
+        Address = address;
+        Port = port;
+    }
+
+    public int Id { get; set; }
+
+    public SourceGame Game { get; set; }
+
+    public string Hostname { get; set; }
+
+    public string DirectoryName { get; set; }
+
+    public string StartupMap { get; set; }
+
+    public int MaxPlayerCount { get; set; }
+
+    public string Address { get; set; }
+
+    public int Port { get; set; }
+
+    public string? LoginToken { get; set; }
+
+    public DateTimeOffset LastSteamCmdRun { get; set; }
+
+    public DateTimeOffset LastSrcdsRun { get; set; }
+}
