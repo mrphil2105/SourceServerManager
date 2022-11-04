24efc1f05d51b39c42f84c8f8d3dd88919f6cec0
Philip Mørch <mrphil2105@gmail.com>
2022-11-04 22:15:33 +0100

    Create modules for services and persistence

---
+++ Src/Presentation/Modules/PersistenceModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SourceServerManager.Infrastructure.Data;
+
+namespace SourceServerManager.Presentation.Modules;
+
+public class PersistenceModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterInstance(new DbContextOptionsBuilder<AppDbContext>().UseSqlite("Data Source=App.db")
+            .Options);
+        builder.RegisterType<AppDbContext>();
+    }
+}
---
+++ Src/Presentation/Modules/ServicesModule.cs
@@ -0,0 +1,13 @@
+﻿using SourceServerManager.Infrastructure.Services;
+
+namespace SourceServerManager.Presentation.Modules;
+
+public class ServicesModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyTypes(typeof(ServerService).Assembly)
+            .Where(t => t.IsInNamespaceOf<ServerService>() && t.IsClass && !t.IsAbstract)
+            .AsImplementedInterfaces();
+    }
+}
