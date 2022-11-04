ba55b855f8052b8e215a5ab67da7046f018162b4
Philip Mørch <mrphil2105@gmail.com>
2022-11-04 22:15:52 +0100

    Create module for mapping

---
+++ Src/Presentation/Modules/MappingModule.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using MapsterMapper;
+using SourceServerManager.Core.Mapping;
+
+namespace SourceServerManager.Presentation.Modules;
+
+public class MappingModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.Register<IMapper>(_ =>
+            {
+                var config = new TypeAdapterConfig();
+                config.Scan(typeof(MappingConfig).Assembly);
+
+                return new Mapper(config);
+            })
+            .SingleInstance();
+    }
+}
