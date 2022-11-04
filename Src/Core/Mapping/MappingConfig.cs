42e8c0529f20386662677521a1294ebac4a18da5
Philip Mørch <mrphil2105@gmail.com>
2022-11-04 22:14:49 +0100

    Enable constructor mapping with MappingConfig

---
+++ Src/Core/Mapping/MappingConfig.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+
+namespace SourceServerManager.Core.Mapping;
+
+public class MappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.Default.MapToConstructor(true);
+    }
+}
