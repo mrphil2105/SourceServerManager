62f432d5ec1b1c8098003e0815dd2d58203c4fd8
Philip Mørch <mrphil2105@gmail.com>
2022-11-04 19:22:11 +0100

    Create design-time AppDbContext factory

---
+++ Src/Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace SourceServerManager.Infrastructure.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<AppDbContext>();
+        builder.UseSqlite("Data Source=App.db");
+
+        return new AppDbContext(builder.Options);
+    }
+}
