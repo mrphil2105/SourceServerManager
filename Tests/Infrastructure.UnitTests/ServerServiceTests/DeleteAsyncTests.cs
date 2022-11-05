02c901fb3063b3b99fde525d7b6fe0654f75212d
Philip Mørch <mrphil2105@gmail.com>
2022-11-05 17:38:28 +0100

    Add unit tests for ServerService.DeleteAsync

---
+++ Tests/Infrastructure.UnitTests/ServerServiceTests/DeleteAsyncTests.cs
@@ -0,0 +1,34 @@
+﻿namespace SourceServerManager.Infrastructure.UnitTests.ServerServiceTests;
+
+public class DeleteAsyncTests
+{
+    [Theory]
+    [InfrastructureData]
+    public async Task DeleteAsync_DeletesServer_GivenValidId([Frozen] AppDbContext dbContext, Server server,
+        ServerService serverService)
+    {
+        dbContext.Servers.Add(server);
+        await dbContext.SaveChangesAsync();
+
+        var result = await serverService.DeleteAsync(server.Id);
+
+        result.Success.Should()
+            .BeTrue();
+    }
+
+    [Theory]
+    [InfrastructureData]
+    public async Task DeleteAsync_ReturnsError_GivenInvalidId([Frozen] AppDbContext dbContext, Server server,
+        ServerService serverService)
+    {
+        dbContext.Servers.Add(server);
+        await dbContext.SaveChangesAsync();
+
+        var result = await serverService.DeleteAsync(server.Id + 1);
+
+        result.Success.Should()
+            .BeFalse();
+        result.Message.Should()
+            .Be("The specified server could not be found.");
+    }
+}
