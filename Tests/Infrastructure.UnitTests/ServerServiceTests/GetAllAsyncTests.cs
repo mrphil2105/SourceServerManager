704c46461cf48080cadade15cfceb01433766c24
Philip Mørch <mrphil2105@gmail.com>
2022-11-05 20:05:53 +0100

    Add unit tests for ServerService.GetAllAsync

---
+++ Tests/Infrastructure.UnitTests/ServerServiceTests/GetAllAsyncTests.cs
@@ -0,0 +1,29 @@
+﻿namespace SourceServerManager.Infrastructure.UnitTests.ServerServiceTests;
+
+public class GetAllAsyncTests
+{
+    [Theory]
+    [InfrastructureData]
+    public async Task GetAllAsync_ReturnsAllServers_WithServers([Frozen] AppDbContext dbContext, List<Server> servers,
+        ServerService serverService)
+    {
+        dbContext.Servers.AddRange(servers);
+        await dbContext.SaveChangesAsync();
+
+        var dtos = await serverService.GetAllAsync();
+
+        dtos.Should()
+            .BeEquivalentTo(servers, o => o.Excluding(s => s.LastSteamCmdRun)
+                .Excluding(s => s.LastSrcdsRun));
+    }
+
+    [Theory]
+    [InfrastructureData]
+    public async Task GetAllAsync_ReturnsEmptyCollection_WithNoServers(ServerService serverService)
+    {
+        var dtos = await serverService.GetAllAsync();
+
+        dtos.Should()
+            .BeEmpty();
+    }
+}
