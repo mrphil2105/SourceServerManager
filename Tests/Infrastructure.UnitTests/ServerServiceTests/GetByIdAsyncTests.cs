ef998b66149f0a0b4906f26b4bbe91be28f0af95
Philip Mørch <mrphil2105@gmail.com>
2022-11-05 20:00:31 +0100

    Add unit tests for ServerService.GetByIdAsync

---
+++ Tests/Infrastructure.UnitTests/ServerServiceTests/GetByIdAsyncTests.cs
@@ -0,0 +1,32 @@
+﻿namespace SourceServerManager.Infrastructure.UnitTests.ServerServiceTests;
+
+public class GetByIdAsyncTests
+{
+    [Theory]
+    [InfrastructureData]
+    public async Task GetByIdAsync_ReturnsMatchingServer_GivenValidId([Frozen] AppDbContext dbContext, Server server,
+        ServerService serverService)
+    {
+        dbContext.Servers.Add(server);
+        await dbContext.SaveChangesAsync();
+
+        var dto = await serverService.GetByIdAsync(server.Id);
+
+        dto.Should()
+            .BeEquivalentTo(server);
+    }
+
+    [Theory]
+    [InfrastructureData]
+    public async Task GetByIdAsync_ReturnsNull_GivenInvalidId([Frozen] AppDbContext dbContext, Server server,
+        ServerService serverService)
+    {
+        dbContext.Servers.Add(server);
+        await dbContext.SaveChangesAsync();
+
+        var dto = await serverService.GetByIdAsync(server.Id + 1);
+
+        dto.Should()
+            .BeNull();
+    }
+}
